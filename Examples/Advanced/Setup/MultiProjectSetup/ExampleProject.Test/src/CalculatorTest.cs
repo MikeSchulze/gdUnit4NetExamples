f6b88d56cec650118c0dcee72b9410221e19609e
Nullpointer <mikeschulze.lpz@gmail.com>
2025-12-02 21:38:31 +0100

    Add multi project example
    # What
    Work in progress
    
    - update projects to Godot 4.5.0
    - adding MultiProjectSetup example

---
+++ Examples/Advanced/Setup/MultiProjectSetup/ExampleProject.Test/src/CalculatorTest.cs
@@ -0,0 +1,15 @@
+﻿namespace GdUnit4.Examples.Advanced.Setup.MultiProjectSetup;
+
+using static Assertions;
+
+[TestSuite]
+public class CalculatorTest
+{
+    [TestCase]
+    public void Add()
+    {
+        var calculator = new Calculator();
+
+        AssertThat(calculator.Add(1, 2)).IsEqual(3);
+    }
+}
