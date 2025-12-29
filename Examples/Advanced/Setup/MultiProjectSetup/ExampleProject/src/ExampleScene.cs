fda20f69cb39e3a394f46c386a9076755b04c869
Nullpointer <mikeschulze.lpz@gmail.com>
2025-12-29 13:30:52 +0100

    Fix scene script loading issues (#20)

--- Examples/Advanced/Setup/MultiProjectSetup/ExampleProject/src/ExampleScene.cs
+++ Examples/Advanced/Setup/MultiProjectSetup/ExampleProject/src/ExampleScene.cs
@@ -4,7 +4,17 @@ using Godot;
 
 public partial class ExampleScene : Control
 {
+    private string state = "Unknown";
+
 #pragma warning disable SA1600
     public static int Check() => 0;
 #pragma warning restore SA1600
+
+    public override void _Ready()
+    {
+        base._Ready();
+        state = "_Ready";
+    }
+
+    public string GetState() => state;
 }
